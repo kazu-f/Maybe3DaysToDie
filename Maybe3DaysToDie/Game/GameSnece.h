4d66836c1913e7cf41893ec642a1432385095f35
kamiguchi-reo <kbc19b07@stu.kawahara.ac.jp>
2021-10-04 12:30:13 +0900

    アセット追加

--- Maybe3DaysToDie/Game/GameSnece.h
+++ Maybe3DaysToDie/Game/GameSnece.h
@@ -3,6 +3,7 @@
 class Player;
 class Stage;
 class GameCamera;
+class PlacementObject;
 
 class GameSnece : public IGameObject
 {
@@ -21,7 +22,9 @@ private:
 	Player* m_Player = nullptr;
 	GameCamera* m_Camera = nullptr;
 	Stage* m_Stage = nullptr;
+	PlacementObject* m_PlacementObject = nullptr;		//設置オブジェクト
 
 	//Samples//
 	CFontRender* m_fontRender = nullptr;	//フォントレンダー。
+
 };
--- Maybe3DaysToDie/Game/PlacementObject/PlacementObject.h
+++ Maybe3DaysToDie/Game/PlacementObject/PlacementObject.h
@@ -1,11 +1,18 @@
 #pragma once
-class PlacementObject
+class PlacementObject:public IGameObject
 {
 public:
 	PlacementObject();
 	~PlacementObject();
 
+	void Awake();
+	bool Start();
+	void Update();
+	void OnDestroy();
 private:
-	
+	prefab::ModelRender* m_ObjectModel = nullptr;		//オブジェクトモデル
+	Vector3 m_pos = Vector3::Zero;		//モデルのポジション
+	Vector3 m_scale = Vector3::One;		//モデルのスケール
+	Quaternion m_qrot = Quaternion::Identity;		//モデルの回転
 };
 
