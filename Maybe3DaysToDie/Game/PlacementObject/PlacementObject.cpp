19edc6036b6034352fc7aca5a328d1b072ba2928
kamiguchi-reo <kbc19b07@stu.kawahara.ac.jp>
2021-10-05 10:51:33 +0900

    始点方向にオブジェクトを移動

--- Maybe3DaysToDie/Game/PlacementObject/PlacementObject.cpp
+++ Maybe3DaysToDie/Game/PlacementObject/PlacementObject.cpp
@@ -25,21 +25,24 @@ bool PlacementObject::Start()
 	ModelInitData m_modelInitData;
 	m_modelInitData.m_tkmFilePath = "Assets/modelData/CubeBlock/woodBlock.tkm";
 	m_ObjectModel->Init(m_modelInitData);
-	//物理オブジェクトの作成
-	CreatePhysics();
 	return true;
 }
 
 void PlacementObject::Update()
 {
+	//オブジェクトを設置する位置を計算
+	CalcObjectPos();
 	//各種セット
 	m_ObjectModel->SetPosition(m_pos);
 	m_ObjectModel->SetRotation(m_qrot);
 	m_ObjectModel->SetScale(m_scale);
 }
 
-void PlacementObject::CreatePhysics()
+void PlacementObject::CalcObjectPos()
 {
-	//メッシュの作成
-	m_StaticCol.CreateMesh(m_pos, m_qrot, m_scale, m_ObjectModel);
+	//視点の位置
+	Vector3 m_Start = MainCamera().GetPosition();
+	//視線方向にポジションを加算
+	m_pos = m_Start;
+	m_pos += MainCamera().GetForward() * m_SetRange;
 }
--- Maybe3DaysToDie/Game/PlacementObject/PlacementObject.h
+++ Maybe3DaysToDie/Game/PlacementObject/PlacementObject.h
@@ -1,4 +1,5 @@
 #pragma once
+
 class PlacementObject:public IGameObject
 {
 public:
@@ -8,15 +9,17 @@ public:
 	bool Start();
 	void Update();
 	void OnDestroy();
+
 	/// <summary>
-	/// 物理オブジェクトを作成
+	/// 設置するオブジェクトの位置を計算
 	/// </summary>
-	void CreatePhysics();
+	void CalcObjectPos();
+
 private:
 	prefab::ModelRender* m_ObjectModel = nullptr;		//オブジェクトモデル
-	CPhysicsStaticObject m_StaticCol;		//静的物理オブジェクト
 	Vector3 m_pos = Vector3::Zero;		//モデルのポジション
 	Vector3 m_scale = Vector3::One;		//モデルのスケール
 	Quaternion m_qrot = Quaternion::Identity;		//モデルの回転
+	const float m_SetRange = 500.0f;		//設置範囲
 };
 
