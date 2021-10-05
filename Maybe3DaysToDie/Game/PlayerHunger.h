40b19e1e811545ed00a683db6cad7e1f07c9706d
(kaito1111) <(kbc19b11@stu.kawahara.ac.jp)>
2021-10-05 14:01:51 +0900

    PlayerHunderクラスを追加

---
+++ Maybe3DaysToDie/Game/PlayerHunger.h
@@ -0,0 +1,28 @@
+#pragma once
+/// <summary>
+/// プレイヤーの空腹度を表すクラス
+/// </summary>
+class PlayerHunger : public IGameObject
+{
+private:
+	/// <summary>
+	/// Newされた時にGameObjectから呼ばれる 
+	/// 初期化用関数
+	/// </summary>
+	/// <returns>trueが返るまでループする</returns>
+	bool Start() override final;
+
+	/// <summary>
+	/// 毎フレーム呼ばれる更新関数
+	/// </summary>
+	void Update()override final;
+
+	/// <summary>
+	/// deleteされるとGameObjectから呼ばれる関数
+	/// </summary>
+	void OnDestroy()override final;
+
+private:
+	prefab::CSpriteRender* m_CrrentSprite = nullptr;
+};
+
