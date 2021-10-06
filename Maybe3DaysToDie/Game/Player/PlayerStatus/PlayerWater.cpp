3a7943e68c6b6ed130cf8bb172997cfcd50eb9a9
(kaito1111) <(kbc19b11@stu.kawahara.ac.jp)>
2021-10-06 14:07:10 +0900

    水分のクラスを追加

---
+++ Maybe3DaysToDie/Game/Player/PlayerStatus/PlayerWater.cpp
@@ -0,0 +1,25 @@
+#include "stdafx.h"
+#include "PlayerWater.h"
+#include "SpriteInitFunction.h"
+
+bool PlayerWater::Start()
+{
+
+	return true;
+}
+
+void PlayerWater::Update()
+{
+}
+
+void PlayerWater::OnDestroy()
+{
+}
+
+void PlayerWater::CurretSpriteInit()
+{
+}
+
+void PlayerWater::FlameSpriteInit()
+{
+}
