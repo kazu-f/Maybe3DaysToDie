685fb61ec7a20260c36b65c396317d68509f4fcd
kamiguchi-reo <kbc19b07@stu.kawahara.ac.jp>
2021-10-15 09:40:35 +0900

    DestructibleObjectに各種パラメータを追加

--- Maybe3DaysToDie/Game/DestructibleObject/DestructibleObject.h
+++ Maybe3DaysToDie/Game/DestructibleObject/DestructibleObject.h
@@ -1,9 +1,28 @@
 #pragma once
+struct ObjectParams {
+	char* BlockName = nullptr;		//名前
+	int Durable = 0;		//耐久値
+	int ObtainAmount = 0;		//入手量
+	//todo [リファクタリング] 適性ツールの型をintからenumにする
+	int AptitudeTool = 0;		//適性ツール
+};
 class DestructibleObject
 {
 public:
 	DestructibleObject() {}
 	virtual ~DestructibleObject() {}
+
+	/// <summary>
+	/// オブジェクトの各種パラメータをセット
+	/// </summary>
+	/// <param name="param">パラメータ</param>
+	void SetParams(ObjectParams&param)
+	{
+		m_params.BlockName = param.BlockName;
+		m_params.Durable = param.Durable;
+		m_params.ObtainAmount = param.ObtainAmount;
+		m_params.AptitudeTool = param.AptitudeTool;
+	}
 
 	/// <summary>
 	/// ポジションをゲット
@@ -32,11 +51,21 @@ public:
 		return m_scale;
 	}
 
+	/// <summary>
+	/// モデルをセット
+	/// </summary>
+	/// <param name="model">モデルレンダーのポインタ</param>
+	void SetModel(prefab::ModelRender* model)
+	{
+		m_model = model;
+	}
+
 
 protected:
-	
+	prefab::ModelRender* m_model = nullptr;
 	Vector3 m_position = Vector3::Zero;
 	Quaternion m_qrot = Quaternion::Identity;
 	Vector3 m_scale = Vector3::One;
+	bool IsDestructed = false;
+	ObjectParams m_params;
 };
-
