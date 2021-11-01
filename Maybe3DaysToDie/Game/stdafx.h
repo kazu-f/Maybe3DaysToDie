4412407e1d9a969c76ebdd2bf687e05d1798362a
kamiguchi-reo <kbc19b07@stu.kawahara.ac.jp>
2021-11-01 14:55:15 +0900

    仮にTerrainWorldを表示

--- Maybe3DaysToDie/Game/stdafx.h
+++ Maybe3DaysToDie/Game/stdafx.h
@@ -5,6 +5,6 @@
 #include "DestructibleObject/DestructibleObject.h"
 
 //チャンクのサイズ
-const int ChunkWidth = 64;		//横幅
-const int ChunkDepth = 64;		//奥行
+const int ChunkWidth = 15;		//横幅
+const int ChunkDepth = 15;		//奥行
 using namespace Engine;
