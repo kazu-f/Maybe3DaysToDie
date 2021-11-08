19b70a66b47b0fabee4fd1e379ef10bf6245cd7c
kamiguchi-reo <kbc19b07@stu.kawahara.ac.jp>
2021-11-08 10:15:17 +0900

    ツールの種類を示す列挙型を追加

--- Maybe3DaysToDie/Game/stdafx.h
+++ Maybe3DaysToDie/Game/stdafx.h
@@ -10,4 +10,13 @@ const int ChunkDepth = 15;		//奥行
 
 const float SetRange = 500.0f;		//プレイヤーが届く範囲
 
+//ツールの種類
+enum ToolKinds {
+	axe = 0,		//斧
+	Pickaxe = 1 << 0,		//ツルハシ
+	shovel = 1 << 1,		//ショベル
+	others = 1 << 2,		//その他
+	DebugTool = 0xffffffff		//デバッグ用ツール
+};
+
 using namespace Engine;
