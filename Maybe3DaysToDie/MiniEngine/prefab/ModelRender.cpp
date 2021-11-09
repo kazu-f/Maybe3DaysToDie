15351bb59ffc857431f4f79dddbb18f9d3213805
kamiguchi-reo <kbc19b07@stu.kawahara.ac.jp>
2021-11-09 11:26:33 +0900

    ModelRenderのAddInstancing関数でモデルの初期化をすることで更新するようにした

--- Maybe3DaysToDie/MiniEngine/prefab/ModelRender.cpp
+++ Maybe3DaysToDie/MiniEngine/prefab/ModelRender.cpp
@@ -117,6 +117,8 @@ namespace Engine {
 			m_instancingDataSB.Init(sizeof(Matrix), m_maxInstance, m_instancingData.get());
 			m_isEnableInstancing = true;
 			UpdateInstancingData(pos, rot, scale);
+			//todo Init呼んで更新してるからできそうならインスタンシングデータのみを更新するように変更
+			m_model.Init(m_modeInitData, m_maxInstance);
 		}
 		void ModelRender::InitInstancing(int maxInstance)
 		{
