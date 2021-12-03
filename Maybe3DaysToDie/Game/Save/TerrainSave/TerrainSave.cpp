0d67ee4e75e49e7cba3358b37635d455dab745de
kamiguchi-reo <kbc19b07@stu.kawahara.ac.jp>
2021-12-03 10:50:35 +0900

    ロード用のフォルダを追加

--- Maybe3DaysToDie/Game/Save/TerrainSave/TerrainSave.cpp
+++ Maybe3DaysToDie/Game/Save/TerrainSave/TerrainSave.cpp
@@ -72,4 +72,13 @@ void TerrainSave::Load()
 	//	}
 	//}
 	fread(&m_ChunkData, sizeof(ChunkData), MAX_CHUNK_SIDE * MAX_CHUNK_SIDE, fp);
+
+	for (int chunk_x = 0; chunk_x < MAX_CHUNK_SIDE; chunk_x++)
+	{
+		for (int chunk_y = 0; chunk_y < MAX_CHUNK_SIDE; chunk_y++)
+		{
+			//ƒ`ƒƒƒ“ƒN‚²‚Æ‚ÉŽæ‚èo‚·
+
+		}
+	}
 }
