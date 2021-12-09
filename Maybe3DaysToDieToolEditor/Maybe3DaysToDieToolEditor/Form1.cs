877d7eb445f1725a26346c9cd80d98478b8e5572
kazu-f <kbc19b20@stu.kawahara.ac.jp>
2021-12-09 15:10:50 +0900

    C#プロジェクト入れ直し。

---
+++ Maybe3DaysToDieToolEditor/Maybe3DaysToDieToolEditor/Form1.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Maybe3DaysToDieToolEditor
+{
+    public partial class Maybe3DaysToDie_ToolEditor : Form
+    {
+        BindingList<Item> itemList = new BindingList<Item>();
+        BindingSource bs = new BindingSource();
+        public Maybe3DaysToDie_ToolEditor()
+        {
+            InitializeComponent();
+            bs.DataSource = itemList;
+
+            ItemList.DisplayMember = "itemName";
+            ItemList.ValueMember = "itemName";
+            ItemList.DataSource = bs;
+        }
+
+        /// <summary>
+        /// リストに新しいツールデータを追加する。
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void ToolDataToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            var newTool = new ToolData { itemName = "ToolData" };
+            itemList.Add(newTool);
+
+            bs.ResetBindings(false);
+        }
+
+        /// <summary>
+        /// リストの選択が変更されたとき。
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void ItemList_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            NameTextBox.Text = ((Item)ItemList.SelectedItem).itemName;
+        }
+
+        /// <summary>
+        /// テキストボックスの中が変更されたとき。
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void NameTextBox_TextChanged(object sender, EventArgs e)
+        {
+            ((Item)ItemList.SelectedItem).itemName = NameTextBox.Text;
+            bs.ResetBindings(false);
+        }
+    }
+}
---
+++ Maybe3DaysToDieToolEditor/Maybe3DaysToDieToolEditor/ToolData.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Json;
+
+namespace Maybe3DaysToDieToolEditor
+{
+    /// <summary>
+    /// アイテムの基本的な情報。
+    /// </summary>
+    [DataContract]
+    abstract class Item 
+    {
+        [DataMember(Name = "name")]
+        public string itemName { get; set; }
+    }
+
+    public enum ToolKinds : uint
+    {
+        hand = 0,                //素手
+        axe = 1 << 0,            //斧
+        Pickaxe = 1 << 1,        //ツルハシ
+        shovel = 1 << 2,         //ショベル
+        others = 1 << 3,         //その他
+        DebugTool = 0xffffffff,  //デバッグ用ツール
+    };
+
+    /// <summary>
+    /// ツールの情報。
+    /// </summary>
+    [DataContract]
+    class ToolData : Item
+    {
+        public int damage { get; set; } = 0;
+        public int durable { get; set; } = 0;
+        public int useStamina { get; set; } = 0;
+        public int tool { get; set; } = 0;
+    }
+}
