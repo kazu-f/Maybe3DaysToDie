0db5b13ca6a36920ace37b70fcb78593cc47ba7a
(kaito1111) <(kbc19b11@stu.kawahara.ac.jp)>
2022-01-19 15:39:28 +0900

    リファクタリング

--- Maybe3DaysToDie/Game/Status/Inventory.h
+++ Maybe3DaysToDie/Game/Status/Inventory.h
@@ -9,13 +9,34 @@ public:
 	void SetPlayer(Player* pp) {
 		m_player = pp;
 	}
+	/// <summary>
+	/// アイテムをスロットのどこにセットする？
+	/// </summary>
+	/// <param name="Item">アイテム</param>
+	/// <param name="x">ｘ座標</param>
+	/// <param name="y">ｙ座標</param>
+	void SetItemSlot(GameItemBase* Item,const int x, const int y) {
+		m_ItemSlot[x][y] = Item;
+	}
 private:
 	bool Start()override;
 	void Update()override;
 	void OnDestroy()override;
 
+	LRESULT  CALLBACK  WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam);
+	/// <summary>
+	/// インベントリの状態に遷移するかの判定をする
+	/// </summary>
+	void SwhichInventoryState();
+	/// <summary>
+	/// Tabのトリガー判定を取る
+	/// </summary>
+	void TriggerTab();
 	prefab::CSpriteRender* m_Inbentory = nullptr;
 	bool m_IsShow = false;
 	bool m_IsTriggerTab = false;
 	Player* m_player = nullptr;
+	GameItemBase* m_ItemSlot[7][6] = { nullptr };
+
+	POINT m_Pt;							//マウスカーソルの座標
 };
