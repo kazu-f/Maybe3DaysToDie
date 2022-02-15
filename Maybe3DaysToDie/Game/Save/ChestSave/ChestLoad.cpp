577e60a22579e0ef0b09055262de086770e2236b
kamiguchi-reo <kbc19b07@stu.kawahara.ac.jp>
2022-02-15 18:56:07 +0900

    Chestのデータをセーブロードするクラスを作成

--- Maybe3DaysToDie/Game/Save/ChestSave/ChestLoad.cpp
+++ Maybe3DaysToDie/Game/Save/ChestSave/ChestLoad.cpp
@@ -1,2 +1,36 @@
 #include "stdafx.h"
 #include "ChestLoad.h"
+
+bool ChestLoad::Load()
+{
+	if (m_dataFile == nullptr)
+	{
+		//セーブデータファイルクラスが参照できませんでした。
+		return false;
+	}
+	FILE* fp;
+	fp = fopen(m_dataFile->GetSaveDataFilePath_Num(), "rb");
+	if (fp == NULL)
+	{
+		//セーブデータがありません
+		return false;
+	}
+
+	FILE* fp2;
+	fp2 = fopen(m_dataFile->GetSaveDataFilePath(), "rb");
+	if (fp2 == NULL)
+	{
+		//セーブデータがありません
+		return false;
+	}
+
+	int size = 0;
+	fread(&size, sizeof(int), 1, fp);
+	m_dataFile->GetAllData().resize(size);
+	fclose(fp);
+
+	fread(&m_dataFile->GetAllData().begin(), sizeof(ChestDataFile::WorldChestData), MAX_CHUNK_SIDE * MAX_CHUNK_SIDE, fp2);
+	//取得できた
+	fclose(fp2);
+	return true;
+}
