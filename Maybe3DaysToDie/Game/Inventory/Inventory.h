676b80422ad1f6bc5cc90f6f7eb7e4b599df6865
(kaito1111) <(kbc19b11@stu.kawahara.ac.jp)>
2022-02-16 18:14:01 +0900

    アイテムの配列を定数にした

--- Maybe3DaysToDie/Game/Inventory/Inventory.h
+++ Maybe3DaysToDie/Game/Inventory/Inventory.h
@@ -39,8 +39,7 @@ private:
 	bool m_IsShow = false;
 	bool m_IsTriggerTab = false;
 	Player* m_player = nullptr;
-	const Vector2 SlotMax = { 7,6 };
-	InventoryItemData m_ItemSlot[7][6];
+	InventoryItemData m_ItemSlot[Inventory_X][Inventory_Y];
 	InventoryItemData m_PickUpItem;
 	RECT m_MainRt;				//ウィンドウ画面
 	RECT m_DeskRt;				//デスクトップ画面
